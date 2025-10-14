bdad132a0220e523458776734a801a438950e08e
HalloweenSchlossAlsbach <flowfabs@gmail.com>
2025-10-14 16:30:29 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,6 @@
-// ### GEÄNDERT: Cache-Version erhöht ###
-const CACHE_NAME = 'event-info-cache-v5';
+// Cache-Version erhöht
+const CACHE_NAME = 'event-info-cache-v6';
 
-// ### GEÄNDERT: Alle Pfade auf "Event-Guide" aktualisiert ###
 const urlsToCache = [
     '/Event-Guide/',
     '/Event-Guide/index.html',
@@ -10,26 +9,24 @@ const urlsToCache = [
     '/Event-Guide/logo.png',
     '/Event-Guide/Lageplan_full.png',
     '/Event-Guide/Lageplan_low.png',
-    '/Event-Guide/Speisekarte_full.png',
-    '/Event-Guide/Speisekarte_low.png',
+    /* ### NEU: Regelwerk-Bilder zum Cache hinzufügen ### */
+    '/Event-Guide/Regelwerk_full.png',
+    '/Event-Guide/Regelwerk_low.png',
     '/Event-Guide/manifest.json',
     '/Event-Guide/HalloweenSchrift.otf',
     'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css'
 ];
 
-// Installation des Service Workers
 self.addEventListener('install', event => {
     event.waitUntil(
         caches.open(CACHE_NAME)
             .then(cache => {
                 console.log('Cache geöffnet');
-                // Wichtig: Auch die logo.png zum Cache hinzufügen
                 return cache.addAll(urlsToCache);
             })
     );
 });
 
-// Anfragen abfangen und aus dem Cache bedienen
 self.addEventListener('fetch', event => {
     event.respondWith(
         caches.match(event.request)
